5ea683184f2a989ef9ce63a8f0e0f6bf3fcc1cf5
dkijania <dariusz@o1labs.org>
2023-12-06 23:59:32 +0100

    fix checkout path

--- buildkite/scripts/hardfork-tests.sh
+++ buildkite/scripts/hardfork-tests.sh
@@ -5,6 +5,8 @@ set -eo pipefail
 # Don't prompt for answers during apt-get install
 export DEBIAN_FRONTEND=noninteractive
 
+git config --global --add safe.directory $BUILDKITE_BUILD_CHECKOUT_PATH
+
 apt-get update
 apt-get install -y git apt-transport-https ca-certificates tzdata curl python3 python3-pip wget
 
