d86b583a32f89d439c00e82eea961cb096c7c6ca
dkijania <dariusz@o1labs.org>
2023-12-07 18:21:02 +0100

    set git env vars

--- buildkite/scripts/hardfork-tests.sh
+++ buildkite/scripts/hardfork-tests.sh
@@ -6,6 +6,7 @@ set -eo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 git config --global --add safe.directory $BUILDKITE_BUILD_CHECKOUT_PATH
+source buildkite/scripts/export-git-env-vars.sh
 
 apt-get update
 apt-get install -y git apt-transport-https ca-certificates tzdata curl python3 python3-pip wget
