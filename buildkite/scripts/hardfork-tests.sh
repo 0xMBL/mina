cdb1fa451b7cc6395b67fc8c2b7959ac8df4e4e5
dkijania <dariusz@o1labs.org>
2023-12-07 01:16:42 +0100

    fix rest of test

--- buildkite/scripts/hardfork-tests.sh
+++ buildkite/scripts/hardfork-tests.sh
@@ -10,6 +10,12 @@ git config --global --add safe.directory $BUILDKITE_BUILD_CHECKOUT_PATH
 apt-get update
 apt-get install -y git apt-transport-https ca-certificates tzdata curl python3 python3-pip wget
 
+echo "deb [trusted=yes] http://packages.o1test.net bullseye ${MINA_DEB_RELEASE}" | tee /etc/apt/sources.list.d/mina.list
+apt-get update
+
+echo "Installing mina test suite package: mina-test-suite=${MINA_DEB_VERSION}"
+apt-get install --allow-downgrades -y mina-test-suite=${MINA_DEB_VERSION}
+
 source buildkite/scripts/export-git-env-vars.sh
 
 CONFIG=/etc/mina/test/hardfork_tests/ci.json
