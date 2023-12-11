60c9d6f4aaf11cfffba06ba4c18a9e7d9ea09cfe
dkijania <dariusz@o1labs.org>
2023-12-11 18:43:42 +0100

    fix run test script and fix docker entrypoint

--- buildkite/scripts/hardfork-archive-migration-tests.sh
+++ buildkite/scripts/hardfork-archive-migration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#git config --global --add safe.directory $BUILDKITE_BUILD_CHECKOUT_PATH
-#source buildkite/scripts/export-git-env-vars.sh
+git config --global --add safe.directory $BUILDKITE_BUILD_CHECKOUT_PATH
+source buildkite/scripts/export-git-env-vars.sh
 
 POSTGRES_PORT=5555
 REFERENCE_COMMIT=c980ba8
@@ -9,20 +9,24 @@ REFERENCE_DOCKER=minaprotocol/mina-archive:1.4.0-$REFERENCE_COMMIT-bullseye
 
 wget https://raw.githubusercontent.com/MinaProtocol/mina/c980ba8/src/app/archive/create_schema.sql
 
+
 docker stop postgres || true && docker rm postgres || true
 docker network create hardfork || true
-docker run --name postgres --network hardfork -p $POSTGRES_PORT:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=archive -v ./create_schema.sql:/docker-entrypoint-initdb.d/init.sql -v .:/src -d postgres
+docker run --name postgres --network hardfork -p $POSTGRES_PORT:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=archive -v $BUILDKITE_BUILD_CHECKOUT_PATH/create_schema.sql:/docker-entrypoint-initdb.d/init.sql  -d postgres:12-bullseye
 
 echo "Waiting for db to init..."
-READY=""
-while [[ $READY != *"accepting connections"* ]]; do
-    READY=$(docker container exec postgres pg_isready -U postgres)
+while true; do
+    if [[ "$(docker container exec postgres pg_isready -U postgres)" == *"accepting connections"* ]]; then
+        break;
+    fi
     sleep 5
 done
-    
-wget -c https://storage.googleapis.com/mina-archive-dumps/mainnet-archive-dump-2023-11-02_0000.sql.tar.gz -O - | tar -xz 
 
-PGPASSWORD=postgres psql -h localhost -p $POSTGRES_PORT -U postgres archive < mainnet-archive-dump-2023-11-02_0000.sql
+wget -c https://storage.googleapis.com/mina-archive-dumps/mainnet-archive-dump-2023-11-02_0000.sql.tar.gz -O - | tar -xz
+
+docker cp  mainnet-archive-dump-2023-11-02_0000.sql postgres:/mainnet-archive-dump-2023-11-02_0000.sql
+docker exec postgres psql -U postgres -c 'create database archive_balances_migrated'
+docker exec postgres psql -U postgres  <  mainnet-archive-dump-2023-11-02_0000.sql
 
 #STATE_HASH=$(psql postgres://postgres:postgres@127.0.0.1:5555/archive_balances_migrated -t -c "select state_hash from blocks where global_slot_since_genesis = 200 and chain_status = 'canonical'" | xargs)
 
@@ -32,13 +36,13 @@ PGPASSWORD=postgres psql -h localhost -p $POSTGRES_PORT -U postgres archive < ma
 
 #docker run --network hardfork --volume .:/workdir minaprotocol/mina-archive:1.4.0-c980ba8-bullseye mina-replayer --archive-uri "postgres://postgres:postgres@172.21.0.1:5555/archive_balances_migrated"  --input /workdir/mainnet_replayer_config.json --output-file /workdir/mainnet_${STATE_HASH}_output.json
 
-PGPASSWORD=postgres psql -h localhost -p $POSTGRES_PORT -U postgres -c "CREATE DATABASE random_mainnet;" 
+docker exec postgres  psql  -U postgres -c "CREATE DATABASE random_mainnet;"
 
-PGPASSWORD=postgres psql -h localhost -p $POSTGRES_PORT -U postgres -d random_mainnet < src/test/hardfork/test_data/random_migration/dump.sql
+docker exec postgres psql -U postgres -d random_mainnet < src/test/hardfork/test_data/random_migration/dump.sql
 
 #STATE_HASH=$(psql postgres://postgres:postgres@127.0.0.1:5555/random_mainnet -t -c "select state_hash from blocks where chain_status = 'canonical' order by global_slot_since_genesis DESC LIMIT 1" | xargs)
 #echo "{ \"genesis_ledger\":$(cat "src/test/hardfork/test_data/random_migration/genesis_ledger.json" | jq '(.ledger)' ), \"target_epoch_ledgers_state_hash\":\"${STATE_HASH}\" }" > random_migrated_replayer_config.json
 
 #docker run --network hardfork --volume .:/workdir minaprotocol/mina-archive:1.4.0-c980ba8-bullseye mina-replayer --archive-uri "postgres://postgres:postgres@172.21.0.1:5555/random_mainnet"  --input /workdir/random_migrated_replayer_config.json --output-file /workdir/random_mainnet_${STATE_HASH}_output.json
 
-docker run minaprotocol/mina-test-suite:1.4.0-$REFERENCE_COMMIT-bullseye min-archive_migration_tests.exe test --env /etc/mina/test/archive_migration_tests/ci.json mainnet_migration
+docker run gcr.io/o1labs-192920/mina-test-suite:$MINA_DOCKER_TAG mina-archive_migration_tests.exe test --env /etc/mina/test/archive_migration_tests/ci.json mainnet_migration
